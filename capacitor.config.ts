5a5053e11d2b1862d656713c72b6df79c9061106
kafribung <60900117001@uin-alauddin.ac.id>
2022-06-09 08:57:28 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'learn-ionic-vue',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
